90f78dddc797ca68daa093d2df82854260f40c74
hujiaju <nanhujiaju@gmail.com>
2018-04-27 10:38:11 +0800

    init ptoject

---
+++ NewEducation/Pods/SBHTTPService/SBHTTPService.swift
@@ -0,0 +1,9 @@
+//
+//  SBHTTPService.swift
+//  NewEducation
+//
+//  Created by nanhu on 2018/4/27.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+import Foundation
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+
+target 'NewEducation' do
+pod 'Alamofire', '~> 4.7.2'
+end
